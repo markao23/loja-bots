08fab4ab9ccd8a8cfbc69aeac1e689440a7b6bd7
markus <markuszimmermann3897@gmail.com>
2026-05-14 17:16:41 -0300

    fix: concertaei o erro de importação pois o go nao tava lendo o .env meu tbm comcertei o erro de leitura colocando o path certo ../../.env se ele nao encontrar ele vai ler direto da raiz coloquei o tratamento de erro pra se ele nao leu o .env

--- backend/internal/database/db.go
+++ backend/internal/database/db.go
@@ -5,16 +5,29 @@ import (
     "fmt"
     "log"
     "os"
+    "github.com/joho/godotenv"
     _ "github.com/lib/pq"
 )
 
 func ConectarBanco() (*sql.DB, error) {
+    err := godotenv.Load("../../.env")
+    if err != nil {
+        err = godotenv.Load(".env")
+        if err != nil {
+            log.Println("Aviso: Arquivo .env não encontrado. Tentando ler variáveis direto do sistema.")
+        }
+    }
+
     // Lendo os dados com segurança de fora do código
     host := os.Getenv("DB_HOST")
     port := os.Getenv("DB_PORT")
     user := os.Getenv("DB_USER")
     password := os.Getenv("DB_PASSWORD")
     dbname := os.Getenv("DB_NAME")
+
+    if host == "" || port == "" || user == "" || dbname == "" {
+        log.Fatal("ERRO CRÍTICO: Variáveis de ambiente do banco de dados não configuradas no .env!")
+    }
 
     // Montando a string dinamicamente
     strConexao := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
